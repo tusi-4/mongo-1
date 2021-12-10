fd4632f3a7d728021625ac5595f93eb4f4af526a
Marta Wojdat <marjow4@gmail.com>
2021-12-10 15:01:05 +0100

    Change messages for routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -53,7 +53,7 @@ router.put('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if(dep) {
       await Department.updateOne({ _id: req.params.id }, { $set: { name: name }});
-      res.json({ message: 'OK' });
+      res.json(dep);
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -67,7 +67,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json(dep);
     }
     else res.status(404).json({ message: 'Not found...' });
   }
